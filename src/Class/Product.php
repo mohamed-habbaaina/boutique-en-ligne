99f00777fa8f80220ee0c1d2c56e96435156fa60
mohamed-habbaaina <mohamed.habbaaina@laplateforme.io>
2023-04-02 12:45:09 +0200

    PHP, class Product: method to get all products limit 6 offset 8/page

--- src/Class/Product.php
+++ src/Class/Product.php
@@ -12,22 +12,29 @@ class Product
 
     /**
      * method to get product by id
+     * @return array fetch_assoc
      */
-    public function getProduct(int $idProduct): null|array
+    public function getProduct(int $idProduct): array
     {
         $sqlIdProduct = 'SELECT * FROM `product` WHERE id_pro=:id LIMIT 1';
-        $dataProcut = \DbConnection::getDB()->prepare($sqlIdProduct);
+        $dataProcut = DbConnection::getDB()->prepare($sqlIdProduct);
         $dataProcut->bindParam(':id', $idProduct);
         $dataProcut->execute();
-        return $dataProcut->fetch();
+        return $dataProcut->fetch(\PDO::FETCH_ASSOC);
 
     }
 
-    // public function getAllProduct(): ?array
-    // {
-// 
-    // }
+    /**
+     * method to get all products limit 6 offset 8/page
+     */
+    public function getAllProduct(int $offset): array
+    {
+        $sqlAllProduct = 'SELECT * FROM `product` LIMIT 8 OFFSET ' . $offset;
+        $dataAllProduct = DbConnection::getDb()->prepare($sqlAllProduct);
+        $dataAllProduct->execute();
+        return $dataAllProduct->fetchAll(\PDO::FETCH_ASSOC);
+    }
 }
 $produit = new Product();
 
-var_dump($produit->getProduct(3));
+var_dump($produit->getAllProduct(0));

329217a75b6b4092ad1f755fe472d48935b19737
mohamed-habbaaina <mohamed.habbaaina@laplateforme.io>
2023-04-03 17:18:19 +0200

    rename the class folder

---
+++ src/Classes/ConnectDb.php
@@ -0,0 +1,25 @@
+<?php
+namespace src\Classes;
+class DbConnection
+{
+    private static ?\PDO $_db = null;
+
+    public static function getDb()
+    {
+        if (!self::$_db) {
+            try {
+                // get database infos from ini file in config folder
+                $db = parse_ini_file('..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'db.ini');
+
+                // define PDO dsn with retrieved data
+                self::$_db = new \PDO($db['type'] . ':dbname=' . $db['name'] . ';host=' . $db['host'] . ';charset=' . $db['charset'], $db['user'], $db['password']);
+
+                // prevent emulation of prepared requests
+                self::$_db->setAttribute(\PDO::ATTR_EMULATE_PREPARES, false);
+            } catch (\PDOException $e) {
+                echo $e->getMessage();
+            }
+        }
+        return self::$_db;
+    }
+}
---
+++ src/Classes/Product.php
@@ -0,0 +1,46 @@
+<?php
+namespace src\Classes;
+
+require_once('../Classes/ConnectDb.php');
+
+class Product
+{
+    public function __construct()
+    {
+
+    }
+
+    /**
+     * method to get product by id
+     * @return array fetch_assoc
+     */
+    public function getProduct(int $idProduct): array
+    {
+        $sqlIdProduct = 'SELECT * FROM `product` WHERE id_pro=:id LIMIT 1';
+        $dataProcut = DbConnection::getDB()->prepare($sqlIdProduct);
+        $dataProcut->bindParam(':id', $idProduct);
+        $dataProcut->execute();
+        return $dataProcut->fetch(\PDO::FETCH_ASSOC);
+
+    }
+
+    /**
+     * method to get all products limit 6 offset 8/page
+     */
+    public function getAllProduct(int $offset): array
+    {
+        $sqlAllProduct = 'SELECT product.id_pro, name_pro, price_pro, image_pro, category_pro, AVG(value_rat) as avg_rating 
+        FROM `product` 
+        LEFT JOIN `rate` 
+        ON product.id_pro = rate.id_pro 
+        GROUP BY product.id_pro 
+        LIMIT 8 OFFSET ' . $offset;
+
+        $dataAllProduct = DbConnection::getDb()->prepare($sqlAllProduct);
+        $dataAllProduct->execute();
+        return $dataAllProduct->fetchAll(\PDO::FETCH_ASSOC);
+    }
+}
+// $produit = new Product();
+
+// var_dump($produit->getAllProduct(0));

d0cf13b45379b68aae039044c267caaea7115136
mohamed-habbaaina <mohamed.habbaaina@laplateforme.io>
2023-04-04 10:46:33 +0200

    fixed bug connection in product page !

--- src/Classes/DbConnection.php
+++ src/Classes/DbConnection.php
@@ -2,6 +2,10 @@
 namespace src\Classes;
 class DbConnection
 {
+    private static $servername = 'localhost';
+    private static $username_b = 'root';
+    private static $password_b = '';
+    private static $database = 'boutique_en_ligne';
     private static ?\PDO $_db = null;
 
     public static function getDb()
@@ -9,10 +13,12 @@ class DbConnection
         if (!self::$_db) {
             try {
                 // get database infos from ini file in config folder
-                $db = parse_ini_file('..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'db.ini');
+                // $db = parse_ini_file('..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'db.ini');
 
                 // define PDO dsn with retrieved data
-                self::$_db = new \PDO($db['type'] . ':dbname=' . $db['name'] . ';host=' . $db['host'] . ';charset=' . $db['charset'], $db['user'], $db['password']);
+                // self::$_db = new \PDO($db['type'] . ':dbname=' . $db['name'] . ';host=' . $db['host'] . ';charset=' . $db['charset'], $db['user'], $db['password']);
+
+                self::$_db = new \PDO('mysql:dbname=' . self::$database . ';host=' . self::$servername . ';charset=utf8mb4', self::$username_b, self::$password_b);
 
                 // prevent emulation of prepared requests
                 self::$_db->setAttribute(\PDO::ATTR_EMULATE_PREPARES, false);

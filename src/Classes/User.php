e45077183471b0ffe610022555c31290bde6d4af
Guangquan Ye <shinz@MacBook-Pro-de-Guangquan.local>
2023-04-04 16:16:37 +0200

    modif de User.php

--- src/Classes/User.php
+++ src/Classes/User.php
@@ -1,6 +1,5 @@
 <?php
 
-
 require_once('DbConnection.php');
 
 Class User
--- view/connect.php
+++ view/connect.php
@@ -1,26 +1,15 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
 <form methode="post" id="logForm">
-                    
-                    <div class="input_contener">
-                        <input type="text" name="logEmail" id="logEmail" placeholder=" ">
-                        <label for="email">Mail</label>
-                    </div>
-                    <div class="input_contener">
-                        <input type="password" name="logPassword" id="logPassword" placeholder=" ">
-                        <label for="password">Password</label>
-                    </div>
-                    <div id="submit_form">
-                        <button type="submit" id="logBtn">Connection</button>
-                    </div>
-                </form>
-    <script src="./js/auth.js"></script>
-</body>
-</html>
+
+    <div class="input_contener">
+        <input type="text" name="logEmail" id="logEmail" placeholder=" ">
+        <label for="email">Mail</label>
+    </div>
+    <div class="input_contener">
+        <input type="password" name="logPassword" id="logPassword" placeholder=" ">
+        <label for="password">Password</label>
+    </div>
+    <div id="submit_form">
+        <button type="submit" id="logBtn">Connection</button>
+    </div>
+</form>
+<script src="./js/auth.js"></script>

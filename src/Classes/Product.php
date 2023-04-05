008ca22730e1b55ca714abab904d43f234aa493b
mohamed-habbaaina <mohamed.habbaaina@laplateforme.io>
2023-04-05 11:49:29 +0200

    PHP, class Product, method: ceratNewImageName, getLasId, insertProduct

--- src/Classes/Product.php
+++ src/Classes/Product.php
@@ -44,7 +44,50 @@ class Product
         $dataAllProduct->execute();
         return $dataAllProduct->fetchAll(\PDO::FETCH_ASSOC);
     }
+
+
+    /******************* Image name ********************/
+    /***************************************************/
+
+    // Get the last id in database
+    public function getLastId()
+    {
+        $sqlLastId = 'SELECT `id_pro` FROM `product` ORDER BY id_pro DESC LIMIT 1';
+        $lastId = DbConnection::getDb()->prepare($sqlLastId);
+        $lastId->execute();
+        return $lastId->fetch();
+
+    }
+
+    /* Creation of the image name that match the insert image id
+    * eg: if last id = 20 => image name === 21 , image name === next id.
+    */
+    public function creatImageNewName(): string
+    {
+        $lastId = $this->getLastId();
+        return (string)($lastId[0] + 1);
+    }
+
+    /**
+     * add products to database
+     */
+    public function insertProduct(string $name, string $description, int $price, string $image, string $origin, string $category): void
+    {
+        $sqlInsert = 'INSERT INTO `product`(`name_pro`, `description_pro`, `price_pro`, `image_pro`, `origin_pro`, `category_pro`) VALUES (:name_pro, :description_pro, :price_pro, :image_pro, :origin_pro, :category_pro)';
+        $reqInsertProduct = DbConnection::getDb()->prepare($sqlInsert);
+        $reqInsertProduct->bindParam(':name_pro', $name);
+        $reqInsertProduct->bindParam(':description_pro', $description);
+        $reqInsertProduct->bindParam(':price_pro', $price);
+        $reqInsertProduct->bindParam(':image_pro', $image);
+        $reqInsertProduct->bindParam(':origin_pro', $origin);
+        $reqInsertProduct->bindParam(':category_pro', $category);
+        $reqInsertProduct->execute();
+
+    }
 }
 // $produit = new Product();
 
-// var_dump($produit->getAllProduct(0));
+// $data = $produit->getImageNewName();
+// var_dump($data);
+
+// $produit->insertProduct('nameName', 'descriptiontest', 10, '1000', 'origin', 'categorie');

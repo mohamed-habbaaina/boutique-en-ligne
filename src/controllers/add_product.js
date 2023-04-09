0da15da01b148646dc4bf2d5232deb87f8eae064
mohamed-habbaaina <mohamed.habbaaina@laplateforme.io>
2023-04-09 13:11:39 +0200

    JS, controller add Product

---
+++ src/controllers/add_product.js
@@ -0,0 +1,76 @@
+/******************* Validate Image ****************/
+/***************************************************/
+
+const formAddProduct = document.forms['addProduct'];
+const displayMessage = document.getElementById('displayMessage');
+let image = formAddProduct['image'];
+let validImage = false;
+
+image.addEventListener('change', e => {
+
+    let file = e.target.files[0];
+    let fileName = file.name;
+    let fileSize = file.size;
+    let fileType = file.type.toLowerCase();
+    const fileExtention = fileName.split('.');
+    const typeImagePermit = ['image/jpeg', 'image/png', 'image/gif', 'image/bmp', 'image/jpg'];
+    let small = image.nextElementSibling;
+
+    // validate size
+    if(fileSize < 2000000)
+    {
+        // validate double extensions.
+        if(fileExtention.length === 2)
+        {
+            // validate type.
+            if(typeImagePermit.includes(fileType))
+            {
+                small.innerHTML = 'Image valide !';
+                small.style.color = 'green';
+
+                validImage = true // validate image
+
+            } else
+            {
+                small.innerHTML = 'Veuillez télécharger une image !';
+                small.style.color = 'red';   
+            }
+        } else{
+            small.innerHTML = 'Veuillez télécharger une image !';
+            small.style.color = 'red';
+        }
+
+    } else
+    {
+        small.innerHTML = 'Taille maximum autorisée 2 M !';
+        small.style.color = 'red';
+    }
+})
+
+/********************** Fetch API **************************/
+/***********************************************************/
+
+formAddProduct.addEventListener('submit', async e => {
+    e.preventDefault();
+     let formData = new FormData(formAddProduct);
+
+    if(validImage)
+    {
+        let response = await fetch('../src/model/add_product.php', {
+            method: 'POST',
+            headers: {
+              'Accept': 'application/json',
+            //   'Content-Type': 'application/json'
+            },
+            body: formData
+        });
+
+        let data = await response.json();
+
+
+
+        displayMessage.innerHTML = data;
+        displayMessage.style.color = 'orange';
+
+    }
+})

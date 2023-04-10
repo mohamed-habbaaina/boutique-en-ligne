aaebc5f9e0367da446c8bdcbd370d5091705da7e
mohamed-habbaaina <mohamed.habbaaina@laplateforme.io>
2023-04-10 23:21:44 +0200

    HTML, index: search bar

--- view/index.php
+++ view/index.php
@@ -1,22 +1,25 @@
-<?php
-session_start();
-?>
 <!DOCTYPE html>
 <html lang="fr">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style/style.css">
     <link rel="stylesheet" href="style/contact.css">
     <link rel="stylesheet" href="style/includes.css">
+    <script defer src="./../src/controllers/searchProduct.js"></script>
     <title>Home</title>
 </head>
-
 <body>
     <?php require_once "./includes/header.php" ?>
-    <div id="formDisplay">
+
+    <main>
+        <div id="search">
+
+            <form action="./../src/controllers/searchProduct.js" method="get" id="searchForm">
+                <input type="search" name="search" placeholder="Rechercher un Produit ...">
+            </form>
+
+            <div id="displayResult">
         <div class="title">
             <h1>START YOUR DAY WITH OUR COFFEE</h1>
         </div>
@@ -24,7 +27,11 @@ session_start();
             <img src="../view/img/table1.jpeg" alt="">
         </div>
     </div>
-    <script src="./js/auth.js"></script>
+
+        </div>
+    </main>
+
+    <?php require_once "./includes/footer.php" ?>
 </body>
 
 </html>

0b57e05a26e459e77503b7f4e0b73de3b7b34604
mohamed-habbaaina <mohamed.habbaaina@laplateforme.io>
2023-04-10 23:21:44 +0200

    HTML, index: search bar

--- view/index.php
+++ view/index.php
@@ -1,18 +1,37 @@
+<?php
+session_start();
+// echo '<pre>';
+// var_dump($_SERVER);
+// echo '<pre>';
+
+?>
 <!DOCTYPE html>
 <html lang="fr">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="./img/favicon.ico" type="image/x-icon">
     <link rel="stylesheet" href="style/contact.css">
     <link rel="stylesheet" href="style/includes.css">
+    <script defer src="./../src/controllers/searchProduct.js"></script>
     <title>Home</title>
 </head>
-
 <body>
     <?php require_once "./includes/header.php" ?>
-    <div id="formDisplay"></div>
-    <script src="./js/auth.js"></script>
+
+    <main>
+        <div id="search">
+
+            <form action="./../src/controllers/searchProduct.js" method="get" id="searchForm">
+                <input type="search" name="search" placeholder="Rechercher un Produit ...">
+            </form>
+
+            <div id="displayResult"></div>
+
+        </div>
+    </main>
+
+    <?php require_once "./includes/footer.php" ?>
 </body>
 </html>

50bfa73bc06eb042cf25a2042b91287c3637d671
Guangquan Ye <shinz@MacBook-Pro-de-Guangquan.local>
2023-04-12 14:31:35 +0200

    fixed index

--- view/index.php
+++ view/index.php
@@ -1,22 +1,13 @@
-<<<<<<< HEAD
-=======
 <?php
 session_start();
 
 ?>
->>>>>>> main
 <!DOCTYPE html>
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<<<<<<< HEAD
-=======
-
-    <link rel="stylesheet" href="style/style.css">
-
->>>>>>> main
     <link rel="stylesheet" href="style/contact.css">
     <link rel="stylesheet" href="style/includes.css">
     <script defer src="./../src/controllers/searchProduct.js"></script>
@@ -25,18 +16,8 @@ session_start();
 <body>
     <?php require_once "./includes/header.php" ?>
 
-<<<<<<< HEAD
     <main>
-        <div id="search">
-
-            <form action="./../src/controllers/searchProduct.js" method="get" id="searchForm">
-                <input type="search" name="search" placeholder="Rechercher un Produit ...">
-            </form>
-
-            <div id="displayResult">
-=======
     <div id="formDisplay">
->>>>>>> main
         <div class="title">
             <h1>START YOUR DAY WITH OUR COFFEE</h1>
         </div>
@@ -44,16 +25,9 @@ session_start();
             <img src="../view/img/table1.jpeg" alt="">
         </div>
     </div>
-<<<<<<< HEAD
 
         </div>
-    </main>
 
-    <?php require_once "./includes/footer.php" ?>
-=======
-    
-
-    <main>
         <div id="search">
 
             <form action="./../src/controllers/searchProduct.js" method="get" id="searchForm">
@@ -68,7 +42,6 @@ session_start();
     <?php require_once "./includes/footer.php" ?>
 
 <script src="./js/auth.js"></script>
->>>>>>> main
 </body>
 
 </html>

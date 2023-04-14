f1c88b37697191f4216eafbc3f7d1e8c472ba11e
Guangquan Ye <shinz@MacBook-Pro-de-Guangquan.local>
2023-04-14 14:24:48 +0200

    add some features admin

--- view/js/admin.js
+++ view/js/admin.js
@@ -53,51 +53,25 @@ function fetchUser() {
             });
             table.appendChild(headerRow);
 
-            // Ajouter chaque ligne de données au tableau
-            const rows = content.map(user => {
-                const row = document.createElement('tr');
-            
-                const firstnameCell = document.createElement('td');
-                firstnameCell.textContent = user.firstname;
-                row.appendChild(firstnameCell);
+        // Ajouter chaque ligne de données au tableau
+        const rows = content.map(user => {
+            const row = document.createElement('tr');
+            const firstnameCell = document.createElement('td');
+            firstnameCell.textContent = user.firstname;
+            row.appendChild(firstnameCell);
+            const lastnameCell = document.createElement('td');
+            lastnameCell.textContent = user.lastname;
+            row.appendChild(lastnameCell);
+            const emailCell = document.createElement('td');
+            emailCell.textContent = user.email;
+            row.appendChild(emailCell);
+            return row;
+        });
+        rows.forEach(row => table.appendChild(row));
 
-                const lastnameCell = document.createElement('td');
-                lastnameCell.textContent = user.lastname;
-                row.appendChild(lastnameCell);
-
-                const emailCell = document.createElement('td');
-                emailCell.textContent = user.email;
-                row.appendChild(emailCell);
-
-                const infoBtnCell = document.createElement('td');
-                const infoBtn = document.createElement('button');
-
-                infoBtn.classList.add('infoBtn');
-                infoBtn.textContent = "Info";
-                infoBtn.type="submit";
-                infoBtn.value = user.id_user;
-                infoBtnCell.appendChild(infoBtn);
-                row.appendChild(infoBtnCell);
-
-                return row;
-            });
-            rows.forEach(row => table.appendChild(row));
-            
-            
-            
-            // Ajouter le tableau au document
-            document.body.appendChild(table);
-
-            getInfoBtn = document.querySelectorAll(".infoBtn");
-            
-
-            getInfoBtn.forEach(getInfo => getInfo.addEventListener("click" , (event)=>{
-                
-                    window.location = "./adminUserInfo.php?userId=" + event.currentTarget.value;
-                 
-                }));
-            
-        })
+        // Ajouter le tableau au document
+        document.body.appendChild(table);
+    })
 
         
 

9eb12098832e6d361279f5244ec903892ec28301
mohamed-habbaaina <mohamed.habbaaina@laplateforme.io>
2023-04-20 18:23:36 +0200

    JS, controller get cart, resolve bug if cart empty

--- src/controllers/get_cart.js
+++ src/controllers/get_cart.js
@@ -1,27 +1,31 @@
-window.addEventListener('DOMContentLoaded', e => {
+window.addEventListener('DOMContentLoaded', async e => {
 const displayCart = document.querySelector('.displayCart');
-let html;
 
-fetch('./../src/model/getCart.php')
-.then(response => response.json())
-.then(data => {
+let html = '';
 
-    if(data)
+let response = await fetch('./../src/model/getCart.php')
+
+    let data = await response.json()
+
+    // check if cart is't empty.
+    if(data != 'empty id_cart')
     {
 
+        let id_cart;
         let total = 0;
         data.forEach(item => {
 
-            console.log(data);
-            let = id_cart = item.id_cart;
+            id_cart = item.id_cart;
 
+            
             let price = item.price_pro / 100;
+            let totalProduct = price * item.quantity;
             total += price * item.quantity;
             html += `<div class="cart">
                         <p class="displayMessageCart"></p>
                         <a href="product.php?idProduct=${item.id_pro}"><img src="./../uploads/${item.image_pro}" alt="${item.name_pro}"></a>
                         <div class="detailProduct">
-                            <h3>${item.name_pro}</h3>
+                        <a href="product.php?idProduct=${item.id_pro}"><h3>${item.name_pro}</h3></a>
                             <p>${price} Euro</p>
 
                             <form action="./../src/model/updateCart.php" method="post">
@@ -30,6 +34,7 @@ fetch('./../src/model/getCart.php')
                                 <input type="number" name="product_quantity" value="${item.quantity}">
                                 <input type="submit" name="update_cart" value="Quantity"/>
                             </form>
+                            <p>${totalProduct.toFixed(2)} Euro</p>
                         </div>
                         <div class="btn-cart">
                             <button><a href="../src/model/deleteCart.php?idProduct=${item.id_pro}">Delete</a></button>
@@ -58,5 +63,4 @@ fetch('./../src/model/getCart.php')
     {
         displayCart.innerHTML = 'Votre Panier est vide !';
     }
-})
 })

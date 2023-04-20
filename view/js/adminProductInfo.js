5b8f95971fd86344b0b9808dfa3024a77e5d525b
alban-martinant-de-preneuf <alban.martinant-de-preneuf@laplateforme.io>
2023-04-20 18:01:08 +0200

    reloading when change innformations and fix css bugs

--- view/js/adminProductInfo.js
+++ view/js/adminProductInfo.js
@@ -4,9 +4,18 @@ const forms = document.querySelectorAll('form');
 // Écouter les évènements
 forms.forEach(form => form.addEventListener('submit', (e) => {
     e.preventDefault();
-    const formData = new FormData(form);
+    const formData = new FormData(form);    
+    
     fetch("../src/controllers/productRouter.php", {
         method: 'POST',
         body: formData
+    }).then(r => {
+        if (r.ok) {
+            const main = document.querySelector('main');
+            main.innerText = "updating..."
+            setTimeout(() => {
+                window.location.reload();
+            },1500);
+        }
     })
 }))

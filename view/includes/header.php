ea24b018894f5c6a48814ddda7741028623c5a87
Guangquan Ye <shinz@mbpdeguangquan.home>
2023-04-22 10:37:00 +0200

    minor fix

--- view/includes/header.php
+++ view/includes/header.php
@@ -49,7 +49,25 @@ if (isset($_SESSION["user"])) {
             </form>
             <div id="displayResult"></div>
         </div>
-</nav>
+
+    </nav>
+    <div class="navbar">
+        <div class="container nav-container">
+            <input class="checkbox" type="checkbox" name="" id="" />
+            <div class="hamburger-lines">
+              <span class="line line1"></span>
+              <span class="line line2"></span>
+              <span class="line line3"></span>
+            </div>  
+          <div class="menu-items">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">about</a></li>
+            <li><a href="#">blogs</a></li>
+            <li><a href="#">portfolio</a></li>
+            <li><a href="#">contact</a></li>
+          </div>
+        </div>
+      </div>
 
 <script defer src="./../src/controllers/searchProduct.js" defer></script>
 

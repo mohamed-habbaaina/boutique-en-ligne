2abc91301482034a6c787c8f7e3499ce283deb33
alban-martinant-de-preneuf <alban.martinant-de-preneuf@laplateforme.io>
2023-04-24 16:01:45 +0200

    update header

--- view/includes/header.php
+++ view/includes/header.php
@@ -10,7 +10,6 @@ if (isset($_SESSION["user"])) {
 <nav>
     <div class="left_part">
         <a href="index.php">Home</a>
-        <a href="about.php">About</a>
         <a href="shop.php">Shop</a>
         <?php if (isset($_SESSION["user"])) : ?>
             <a href="cart.php">Cart</a>
@@ -20,20 +19,21 @@ if (isset($_SESSION["user"])) {
         <img src="./img/logo1.png">
     </div>
     <div class="right_part">
-
-        <a href="contact.php">Contact</a>
-
         <div id="menu">
             <ul>
                 <li>
                     <a href="#">Account</a>
                     <ul>
                         <?php if (isset($_SESSION["user"])) : ?>
-                            <li><a href="profil.php">Profil</a></li>
-                            <form method="post">
-                                <li><button name="deco" id="decoBtn">Deconnexion</button></li>
-                            </form>
-                        <?php else : ?>
+                            <?php if (!isset($_SESSION['user']['role']) || $_SESSION['user']['role'] !== 'admin') : ?>
+                                <li><a href="profil.php">Profil</a></li>
+                                <?php else : ?>
+                                    <li><a href="admin.php">Admin</a></li>
+                            <?php endif ?>
+                                <form method="post">
+                                    <li><button name="deco" id="decoBtn">Deconnexion</button></li>
+                                </form>
+                        <?php else : ?>   
                             <li><button id="logDisplayBtn">Login</button></li>
                             <li><button id="regDisplayBtn">Register</button></li>
                         <?php endif ?>
@@ -50,24 +50,24 @@ if (isset($_SESSION["user"])) {
             <div id="displayResult"></div>
         </div>
 
-    </nav>
-    <div class="navbar">
-        <div class="container nav-container">
-            <input class="checkbox" type="checkbox" name="" id="" />
-            <div class="hamburger-lines">
-              <span class="line line1"></span>
-              <span class="line line2"></span>
-              <span class="line line3"></span>
-            </div>  
-          <div class="menu-items">
+</nav>
+<div class="navbar">
+    <div class="container nav-container">
+        <input class="checkbox" type="checkbox" name="" id="" />
+        <div class="hamburger-lines">
+            <span class="line line1"></span>
+            <span class="line line2"></span>
+            <span class="line line3"></span>
+        </div>
+        <div class="menu-items">
             <li><a href="#">Home</a></li>
             <li><a href="#">about</a></li>
             <li><a href="#">blogs</a></li>
             <li><a href="#">portfolio</a></li>
             <li><a href="#">contact</a></li>
-          </div>
         </div>
-      </div>
+    </div>
+</div>
 
 <script defer src="./../src/controllers/searchProduct.js" defer></script>
 

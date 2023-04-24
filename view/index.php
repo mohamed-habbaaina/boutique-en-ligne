72f98ca93d47157b89e06e06e00392c31403f34c
alban-martinant-de-preneuf <alban.martinant-de-preneuf@laplateforme.io>
2023-04-24 16:18:45 +0200

    fix error messages console

--- view/index.php
+++ view/index.php
@@ -12,8 +12,6 @@ session_start();
     <link rel="stylesheet" href="style/includes.css">
     <link rel="stylesheet" href="style/style.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous" />
-
-    <script defer src="./../src/controllers/searchProduct.js"></script>
     <title>Home</title>
 </head>
 

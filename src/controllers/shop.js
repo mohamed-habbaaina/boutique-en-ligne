ea5d83d2d5a9822dda7154a92208fe78c485c324
alban-martinant-de-preneuf <alban.martinant-de-preneuf@laplateforme.io>
2023-04-26 15:17:24 +0200

    fix next/prev button display

--- src/controllers/shop.js
+++ src/controllers/shop.js
@@ -27,12 +27,10 @@ window.addEventListener('DOMContentLoaded', async () =>{
     
             data.forEach(item => {
                 
-                console.log(data);
                 const rating = item.avg_rating;
                 const starRating = getStarRating(rating); 
                 let price = item.price_pro /100 ;
                 price = price.toFixed(2);
-                console.log(item)
                 html += `
                     <div class="displayShop">
                     <a href="./product.php?idProduct=${item.id_pro}"><img src="../uploads/${item.image_pro}" alt="${item.name_pro}"></a>

80fb88d0bf02c70bc76d106b560be18cc36214a7
Guangquan Ye <shinz@MacBook-Pro-de-Guangquan.local>
2023-04-26 11:21:03 +0200

    media query index,about,footer,header

--- view/includes/header.php
+++ view/includes/header.php
@@ -8,40 +8,42 @@ if (isset($_SESSION["user"])) {
 }
 ?>
 <nav>
-    <div class="left_part">
+    <div class="top_part">
+        <div class="logo_part">
+            <img src="./img/logo1.png">
+        </div>
+            <div id="menu">
+                <ul>
+                    <li>
+                        <a href="#"><ion-icon name="person-outline"></ion-icon></a>
+                        <ul>
+                            <?php if (isset($_SESSION["user"])) : ?>
+                                <?php if (!isset($_SESSION['user']['role']) || $_SESSION['user']['role'] !== 'admin') : ?>
+                                    <li><a href="profil.php">Profil</a></li>
+                                <?php else : ?>
+                                    <li><a href="admin.php">Admin</a></li>
+                                <?php endif ?>
+                                <form method="post">
+                                    <li><button name="deco" id="decoBtn">Deconnexion</button></li>
+                                </form>
+                            <?php else : ?>
+                                <li><button id="logDisplayBtn">Login</button></li>
+                                <li><button id="regDisplayBtn">Register</button></li>
+                            <?php endif ?>
+                        </ul>
+                    </li>
+                </ul>
+
+            </div>
+        
+    </div>
+
+    <div class="bottom_part">
         <a href="index.php">Home</a>
         <a href="shop.php">Shop</a>
         <?php if (isset($_SESSION["user"])) : ?>
             <a href="cart.php">Cart</a>
         <?php endif ?>
-    </div>
-    <div class="logo_part">
-        <img src="./img/logo1.png">
-    </div>
-    <div class="right_part">
-        <div id="menu">
-            <ul>
-                <li>
-                    <a href="#">Account</a>
-                    <ul>
-                        <?php if (isset($_SESSION["user"])) : ?>
-                            <?php if (!isset($_SESSION['user']['role']) || $_SESSION['user']['role'] !== 'admin') : ?>
-                                <li><a href="profil.php">Profil</a></li>
-                                <?php else : ?>
-                                    <li><a href="admin.php">Admin</a></li>
-                            <?php endif ?>
-                                <form method="post">
-                                    <li><button name="deco" id="decoBtn">Deconnexion</button></li>
-                                </form>
-                        <?php else : ?>   
-                            <li><button id="logDisplayBtn">Login</button></li>
-                            <li><button id="regDisplayBtn">Register</button></li>
-                        <?php endif ?>
-                    </ul>
-                </li>
-            </ul>
-
-        </div>
         <div id="search">
 
             <form action="./../src/controllers/searchProduct.js" method="get" id="searchForm">
@@ -49,6 +51,9 @@ if (isset($_SESSION["user"])) {
             </form>
             <div id="displayResult"></div>
         </div>
+    </div>
+    
+
 
 </nav>
 <div class="navbar">
@@ -64,18 +69,13 @@ if (isset($_SESSION["user"])) {
             <li><a href="../view/about.php">About</a></li>
             <li><a href="../view/shop.php">Shop</a></li>
             <li><a href="../view/cart.php">Cart</a></li>
-           
             <li><a href="">contact</a></li>
         </div>
     </div>
-    <div class="logo_partburger">
-        <img src="./img/logo1.png">
-    </div>
-
-    
 </div>
 
 <script defer src="./../src/controllers/searchProduct.js" defer></script>
+<script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
 
 <?php if (!isset($_SESSION['user'])) : ?>
     <!-- <script src="./js/auth.js"></script> -->

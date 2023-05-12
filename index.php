d67503230b43e4557a64432919b80d9545b57fdf
alban-martinant-de-preneuf <alban.martinant-de-preneuf@laplateforme.io>
2023-05-12 13:23:17 +0200

    adding index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+require_once __DIR__ . 'vendor/autoload.php';
+
+$router = new AltoRouter();
+
+$router->setBasePath('/boutique-en-ligne');
+
+$router->map('GET', '/', function() {
+    echo "ok";
+});
+
+// match current request url
+$match = $router->match();
+
+// call closure or throw 404 status
+if (is_array($match) && is_callable($match['target'])) {
+    call_user_func_array($match['target'], $match['params']);
+} else {
+    // no route was matched
+    header($_SERVER['SERVER_PROTOCOL'] . ' 404 Not Found');
+}
